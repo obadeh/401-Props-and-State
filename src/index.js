a5da9c757da9c5d7b736944a452b29a596b4643f
obada <obadaquraan@yahoo.com>
2020-02-29 14:36:36 +0200

    initail

--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/style.css';
-// import App from './components/app.js';
+import App from './components/app.js';
 
-const Jj=()=>{
-    return(<h1>hiii</h1>)
-}
 
-class App extends React.Component {
-    render() {
-      return (
-        <React.Fragment>
-          <Jj />
-        
-        </React.Fragment>
-      );
-    }
-  }
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
